3e217231704a587b5f33158d02c82a1588cd0edc
sriram1990usa <60535385+sriram1990usa@users.noreply.github.com>
2024-12-21 16:54:37 +0530

    New (#1)
    * added .gitignore and changed settings.py
    
    * commit to new

--- CarRental/settings.py
+++ CarRental/settings.py
@@ -10,10 +10,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = 'change me' 
+if SECRET_KEY in os.environ: 
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = false
+
 
 ALLOWED_HOSTS = ['*']
 
